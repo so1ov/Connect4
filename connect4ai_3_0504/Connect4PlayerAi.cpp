3bfe1ffdea98c9fffbc2ed80af81382cadc856d1
so1ov <soloviev.artur@gmail.com>
2018-05-12 16:03:42 +0300

    heuristic decision methods are refactored

--- connect4ai_3_0504/Connect4PlayerAi.cpp
+++ connect4ai_3_0504/Connect4PlayerAi.cpp
@@ -3,7 +3,25 @@
 #include "Connect4Game.hpp"
 
 #include "windows.h"
-#include <exception>
+
+static int oppositeDirections[][2][2] =
+{
+	{
+		{ 1, 0 },{ -1, 0 }
+	},
+	{
+		{ 0, 1 },{ 0, -1 }
+	},
+	{
+		{ 1, 1 },{ -1, -1 }
+	},
+	{
+		{ -1, 1 },{ 1, -1 }
+	}
+};
+
+static int numberOfPairs = sizeof(oppositeDirections) / sizeof(oppositeDirections[0]);
+static int pairOfOppositeVectors = sizeof(oppositeDirections[0]) / sizeof(oppositeDirections[0][0]);
 
 Connect4PlayerAi::Connect4PlayerAi(char _ch)
 	: Connect4Player(_ch)
@@ -47,7 +65,12 @@ int Connect4PlayerAi::firstFreeColumn()
 	return -1;
 }
 
-int Connect4PlayerAi::countMyChipsOnDirection(int _x, int _y, int _dx, int _dy)
+int Connect4PlayerAi::sequenceOnDirection(int _x, int _y, int _dx, int _dy)
+{
+	return sequenceOnDirectionForSpecifiedChip(this->chip_, _x, _y, _dx, _dy);
+}
+
+int Connect4PlayerAi::sequenceOnDirectionForSpecifiedChip(char _chip, int _x, int _y, int _dx, int _dy)
 {
 	static char** field = this->attachedGame_->getField();
 	static int size = this->attachedGame_->getSize();
@@ -60,7 +83,7 @@ int Connect4PlayerAi::countMyChipsOnDirection(int _x, int _y, int _dx, int _dy)
 		&&
 		currentY < size && currentY >= 0)
 	{
-		if (this->chip_ == field[currentY][currentX])
+		if (_chip == field[currentY][currentX])
 		{
 			sequence++;
 		}
@@ -76,7 +99,12 @@ int Connect4PlayerAi::countMyChipsOnDirection(int _x, int _y, int _dx, int _dy)
 	return sequence;
 }
 
-int Connect4PlayerAi::maxPossibleSequenceOnThisDirection(int _x, int _y, int _dx, int _dy)
+int Connect4PlayerAi::possibleSequenceOnDirection(int _x, int _y, int _dx, int _dy)
+{
+	return possibleSequenceOnDirectionForSpecifiedChip(this->chip_, _x, _y, _dx, _dy);
+}
+
+int Connect4PlayerAi::possibleSequenceOnDirectionForSpecifiedChip(char _chip, int _x, int _y, int _dx, int _dy)
 {
 	static char** field = this->attachedGame_->getField();
 	static int size = this->attachedGame_->getSize();
@@ -89,7 +117,7 @@ int Connect4PlayerAi::maxPossibleSequenceOnThisDirection(int _x, int _y, int _dx
 		&&
 		currentY < size && currentY >= 0)
 	{
-		if (this->chip_ == field[currentY][currentX] 
+		if (_chip == field[currentY][currentX]
 			||
 			attachedGame_->EmptyCellCharacter == field[currentY][currentX])
 		{
@@ -107,36 +135,75 @@ int Connect4PlayerAi::maxPossibleSequenceOnThisDirection(int _x, int _y, int _dx
 	return sequence;
 }
 
+int Connect4PlayerAi::maxSequence(int _x, int _y)
+{
+	return maxSequenceForSpecifiedChip(this->chip_, _x, _y);
+}
+
+int Connect4PlayerAi::maxSequenceForSpecifiedChip(char _chip, int _x, int _y)
+{
+	int currentSequence;
+	int maxSequence = 0;
+	int dx, dy;
+
+	for (int thisPair = 0; thisPair < numberOfPairs; thisPair++)
+	{
+		currentSequence = 0;
+		for (int thisVector = 0; thisVector < pairOfOppositeVectors; thisVector++)
+		{
+			dx = oppositeDirections[thisPair][thisVector][0];
+			dy = oppositeDirections[thisPair][thisVector][1];
+			currentSequence += sequenceOnDirectionForSpecifiedChip(_chip, _x, _y, dx, dy);
+		}
+		if (currentSequence > maxSequence)
+		{
+			maxSequence = currentSequence;
+		}
+	}
+
+	return maxSequence;
+}
+
+int Connect4PlayerAi::maxPossibleSequence(int _x, int _y)
+{
+	return maxPossibleSequenceForSpecifiedChip(this->chip_, _x, _y);
+}
+
+int Connect4PlayerAi::maxPossibleSequenceForSpecifiedChip(char _chip, int _x, int _y)
+{
+	int currentPossibleSequence;
+	int maxPossibleSequence = 0;
+	int dx, dy;
+
+	for (int thisPair = 0; thisPair < numberOfPairs; thisPair++)
+	{
+		currentPossibleSequence = 0;
+		for (int thisVector = 0; thisVector < pairOfOppositeVectors; thisVector++)
+		{
+			dx = oppositeDirections[thisPair][thisVector][0];
+			dy = oppositeDirections[thisPair][thisVector][1];
+			currentPossibleSequence += possibleSequenceOnDirectionForSpecifiedChip(_chip, _x, _y, dx, dy);
+		}
+		if (currentPossibleSequence > maxPossibleSequence)
+		{
+			maxPossibleSequence = currentPossibleSequence;
+		}
+	}
+
+	return maxPossibleSequence;
+}
+
 int Connect4PlayerAi::decision()
 {
 	static int columns = attachedGame_->getSize();
-	static int oppositeDirections[][2][2] =
-	{
-		{
-			{ 1, 0 },{ -1, 0 }
-		},
-		{
-			{ 0, 1 },{ 0, -1 }
-		},
-		{
-			{ 1, 1 },{ -1, -1 }
-		},
-		{
-			{ -1, 1 },{ 1, -1 }
-		}
-	};
 
 	int row;
 	int dx, dy;
-	int currentColumnCurrentSequence;
 	int currentColumnMaxSequence;
 	int currentColumnMaxPossibleSequence;
-	int currentColumnCurrentPossibleSequence;
 	int bestSequence = 0;
 	int bestSequenceColumn = -1;
 	int buffer;
-	int numberOfPairs = sizeof(oppositeDirections) / sizeof(oppositeDirections[0]);
-	int pair = sizeof(oppositeDirections[0]) / sizeof(oppositeDirections[0][0]);
 
 	for (int column = 0; column < columns; column++)
 	{
@@ -145,28 +212,9 @@ int Connect4PlayerAi::decision()
 		{
 			continue;
 		}
-		currentColumnMaxSequence = 0;
-		currentColumnMaxPossibleSequence = 0;
-		for (int thisPair = 0; thisPair < numberOfPairs; thisPair++)
-		{
-			currentColumnCurrentSequence = 0;
-			currentColumnCurrentPossibleSequence = 0;
-			for (int thisVector = 0; thisVector < pair; thisVector++)
-			{
-				dx = oppositeDirections[thisPair][thisVector][0];
-				dy = oppositeDirections[thisPair][thisVector][1];
-				currentColumnCurrentSequence += countMyChipsOnDirection(column, row, dx, dy);
-				currentColumnCurrentPossibleSequence += maxPossibleSequenceOnThisDirection(column, row, dx, dy);
-			}
-			if (currentColumnMaxSequence < currentColumnCurrentSequence)
-			{
-				currentColumnMaxSequence = currentColumnCurrentSequence;
-			}
-			if (currentColumnMaxPossibleSequence < currentColumnCurrentPossibleSequence)
-			{
-				currentColumnMaxPossibleSequence = currentColumnCurrentPossibleSequence;
-			}
-		}
+		currentColumnMaxSequence = maxSequence(column, row);
+		currentColumnMaxPossibleSequence = maxPossibleSequence(column, row);
+		
 		if (currentColumnMaxSequence >= bestSequence
 			&&
 			currentColumnMaxPossibleSequence + 1 >= attachedGame_->WinSequence)
@@ -190,3 +238,5 @@ int Connect4PlayerAi::decision()
 		return firstFreeColumn();
 	}
 }
+
+
--- connect4ai_3_0504/Connect4PlayerAi.hpp
+++ connect4ai_3_0504/Connect4PlayerAi.hpp
@@ -7,13 +7,24 @@ class Connect4PlayerAi : public Connect4Player
 {
 public:
 	Connect4PlayerAi(char _ch);
+
 private:
 	int makeTurn();
 	int decision();
-	int countMyChipsOnDirection(int, int, int, int);
-	int maxPossibleSequenceOnThisDirection(int, int, int, int);
-	int findFreeRow(int);
+	int findFreeRow(int column);
 	int firstFreeColumn();
+
+	int maxSequence(int _x, int _y);
+	int maxSequenceForSpecifiedChip(char _chip, int _x, int _y);
+
+	int maxPossibleSequence(int _x, int _y);
+	int maxPossibleSequenceForSpecifiedChip(char _chip, int _x, int _y);
+
+	int sequenceOnDirection(int _x, int _y, int _dx, int _dy);
+	int sequenceOnDirectionForSpecifiedChip(char _chip, int _x, int _y, int _dx, int _dy);
+
+	int possibleSequenceOnDirection(int _x, int _y, int _dx, int _dy);
+	int possibleSequenceOnDirectionForSpecifiedChip(char _chip, int _x, int _y, int _dx, int _dy);
 };
 
 #endif
